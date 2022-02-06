7e97b4755983ab6b6f3bff9259acb23fc3b6af93
alexvmar72 <alexvmar@yandex.ru>
2022-02-06 20:16:29 +0300

    Created task 1_3

---
+++ Markov_Alexei_dz_1/task_1_3.py
@@ -0,0 +1,15 @@
+def transform_string(numbers: int) -> str:
+    number = n
+    if n % 10 == 0:
+        return f'{n} процентов'
+    elif n % 10 == 1:
+        return f'{n} процент'
+    elif n % 10 <= 4:
+        return f'{n} процента'
+    elif n % 10 <= 9:
+        return f'{n} процентов'
+    elif n > 10:
+        return f'{n} процентов'
+
+for n in range(1, 101):
+    print(transform_string(n))
