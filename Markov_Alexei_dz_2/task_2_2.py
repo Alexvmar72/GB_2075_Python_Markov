5c01a5a4b027a13bddcfd917411593a4bc0051c8
alexvmar72 <alexvmar@yandex.ru>
2022-02-06 20:15:55 +0300

    Created task 2

---
+++ Markov_Alexei_dz_2/task_2_2.py
@@ -0,0 +1,24 @@
+def convert_list_in_str(list_in: list) -> str:
+    str_out = ' '
+    for idx, word in enumerate(list_in):
+        if word.isdigit() and len(word) == 1:
+            word = '"' + '0' + word[-1] + '"'
+            list_in[idx] = word
+        elif word.isdigit():
+            word = '"' + word + '"'
+            list_in[idx] = word
+        elif ('-' or '+' in word) and len(word) > 1 and len(word) < 3:
+            word = '"' + word[0] + '0' + word[-1] + '"'
+            list_in[idx] = word
+        elif ('-' or '+' in word) and len(word) >= 3:
+            word = '"' + word + '"'
+            list_in[idx] = word
+
+        str_out += word + ' '
+    return str_out
+
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '-50', 'градусов']
+
+result = convert_list_in_str(my_list)
+print(result)
