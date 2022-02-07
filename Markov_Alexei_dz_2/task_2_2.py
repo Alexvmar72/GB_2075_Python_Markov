5772364dccd0ca4c5399f3391325399ec3f1bbc0
alexvmar72 <alexvmar@yandex.ru>
2022-02-07 09:11:33 +0300

    Changed and corrected file

--- Markov_Alexei_dz_2/task_2_2.py
+++ Markov_Alexei_dz_2/task_2_2.py
@@ -7,18 +7,26 @@ def convert_list_in_str(list_in: list) -> str:
         elif word.isdigit():
             word = '"' + word + '"'
             list_in[idx] = word
-        elif ('-' or '+' in word) and len(word) > 1 and len(word) < 3:
+        elif '+' in word and len(word) > 1 and len(word) < 3:
             word = '"' + word[0] + '0' + word[-1] + '"'
             list_in[idx] = word
-        elif ('-' or '+' in word) and len(word) >= 3:
+        elif '-' in word and len(word) > 1 and len(word) < 3:
+            word = '"' + word[0] + '0' + word[-1] + '"'
+            list_in[idx] = word
+        elif '-' in word and len(word) >= 3:
             word = '"' + word + '"'
             list_in[idx] = word
+        elif '+' in word and len(word) >= 3:
+            word = '"' + word + '"'
+            list_in[idx] = word
+
+
 
         str_out += word + ' '
     return str_out
 
 
-my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '-50', 'градусов']
+my_list = ['в', '5', 'часов', '07', 'минут', 'температура', 'воздуха', 'была', '+17', 'градусов']
 
 result = convert_list_in_str(my_list)
 print(result)
