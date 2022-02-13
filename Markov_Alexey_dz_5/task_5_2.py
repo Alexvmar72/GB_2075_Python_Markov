47173b14b7509f4d3ec8c9e332a34778617bf7a2
alexvmar72 <alexvmar@yandex.ru>
2022-02-13 19:52:05 +0300

    Corrected file

--- Markov_Alexey_dz_5/task_5_2.py
+++ Markov_Alexey_dz_5/task_5_2.py
@@ -3,7 +3,7 @@
 from itertools import islice
 n = int(input('Введите число для расчёта: '))
 
-odd_nums = (num for num in range(1, n+1, 2))
+odd_nums = (num for num in range(1, n + 1, 2))
 print(type(odd_nums))
 
 print(*islice(odd_nums, n))
