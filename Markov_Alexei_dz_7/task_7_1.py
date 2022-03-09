fd38cd5b2f019ead69a93fe0788be8708d3518e1
alexvmar72 <alexvmar@yandex.ru>
2022-03-09 19:11:34 +0300

    corected file 7_1

--- Markov_Alexei_dz_7/task_7_1.py
+++ Markov_Alexei_dz_7/task_7_1.py
@@ -1,10 +1,14 @@
 import os
 
+
+def create_folder(name):
+    if not os.path.exists(name):
+        os.mkdir(name)
+        for el in lst_fold:
+            el = os.path.join(name, el)
+            os.mkdir(el)
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 fold_main = os.path.join(BASE_DIR, 'my_project')
 lst_fold = ['settings', 'mainapp', 'adminapp', 'authapp']
-if not os.path.exists(fold_main):
-    os.mkdir(fold_main)
-    for el in lst_fold:
-        el = os.path.join(fold_main, el)
-        os.mkdir(el)
+create_folder(fold_main)
