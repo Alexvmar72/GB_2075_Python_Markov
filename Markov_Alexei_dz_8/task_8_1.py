53e9c9b303b5d31453a0fdbeec2506faf1b79e6b
alexvmar72 <alexvmar@yandex.ru>
2022-04-02 14:00:33 +0300

    corected file dz_8_1

--- Markov_Alexei_dz_8/task_8_1.py
+++ Markov_Alexei_dz_8/task_8_1.py
@@ -1,35 +1,19 @@
-# Написать тело функцию email_parse(email: str), которая при помощи регулярного выражения
-# извлекает имя пользователя и почтовый домен из email адреса и возвращает их в виде словаря.
-# Если адрес не валиден, выбросить исключение ValueError. Пример:
-#
-# $ email_parse('someone@geekbrains.ru')
-# {'username': 'someone', 'domain': 'geekbrains.ru'}
-# $ email_parse('someone@geekbrainsru')
-# Traceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-#   ...
-#     raise ValueError(msg)
-# ValueError: wrong email: someone@geekbrainsru
-# Примечание: подумайте о ситуации, когда некоторые папки уже есть на диске (как быть?);
-#  как лучше хранить конфигурацию этого стартера, чтобы в будущем можно было менять имена папок
-#  под конкретный проект; можно ли будет при этом расширять конфигурацию и хранить данные
-#  о вложенных папках и файлах (добавлять детали)?
-#
-# ВНИМАНИЕ! Используйте стартовый код для своей реализации:
-#
 import re
 
 
 def email_parse(email: str) -> dict:
-    """
-    Парсит переданную email-строку на атрибуты и возвращает словарь
-    :param email: строковое входное значение обрабатываемого email
-    :return: {'username': <значение до символа @>, 'domain': <значение за символом @>} | ValueError
-    """
-    RE_MAIL = re.compile(r'ваше регулярное выражение')
-    pass  # пишите реализацию здесь
+    RE_MAIL = re.compile(r'(?P<username>\w+)@(?P<domain>\w+\.\w{2,})', re.IGNORECASE)
+    subtotal = RE_MAIL.findall(email)
+    if subtotal == [] or email.count("@") > 1:
+        raise ValueError(f'wrong email: {email}')
+    else:
+        subtotal_2 = RE_MAIL.finditer(email)
+        for element in subtotal_2:
+            return element.groupdict()
 
 
 if __name__ == '__main__':
     email_parse('someone@geekbrains.ru')
-    email_parse('someone@geekbrainsru')
+    #email_parse('someone@geekbrainsru')
+    #email_parse('someone@geekbrains.info')
+    email_parse('someone@geekb@rains.rules')
