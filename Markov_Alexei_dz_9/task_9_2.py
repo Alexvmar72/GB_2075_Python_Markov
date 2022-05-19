3b7f86e78bfe55238dc624c1eb97a0a1bde2be2e
Alex Mark <alexvmar@yandex.ru>
2022-05-19 14:16:56 +0300

    add new file

---
+++ Markov_Alexei_dz_9/task_9_2.py
@@ -0,0 +1,12 @@
+class Road:
+
+    def __init__(self, length: int, width: int):
+        self._length = length
+        self._width = width
+
+    def calculate(self, heigth: int = 5, mass_m_2: int = 25) -> int:
+        return int((self._length * self._width * heigth * mass_m_2) / 1000)
+
+if __name__ == '__main__':
+    road = Road(5000, 20)
+    print(f'Для изготовления покрытия дороги нужно {road.calculate()} тонн.')
