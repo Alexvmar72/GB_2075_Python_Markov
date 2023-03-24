fc8651d95a6b0085b9f91fca8eef99333a5bcab1
Alex Mark <alexvmar@yandex.ru>
2023-03-24 15:51:24 +0300

    corrected file 3

--- Markov_Alexei_dz_1/task_1_3.py
+++ Markov_Alexei_dz_1/task_1_3.py
@@ -1,15 +1,16 @@
 def transform_string(numbers: int) -> str:
 
-    if n % 10 == 0:
-        return f'{n} процентов'
-    elif n // 10 == 1:
-        return f'{n} процентов'
-    elif n % 10 == 1:
-        return f'{n} процент'
-    elif n % 10 <= 4:
-        return f'{n} процента'
-    elif n % 10 <= 9:
-        return f'{n} процентов'
+    if numbers % 10 == 0:
+        result = f'{numbers} процентов'
+    elif numbers // 10 == 1:
+        result = f'{numbers} процентов'
+    elif numbers % 10 == 1:
+        result = f'{numbers} процент'
+    elif numbers % 10 <= 4:
+        result = f'{numbers} процента'
+    elif numbers % 10 <= 9:
+        result = f'{numbers} процентов'
+    return result
 
 
 for n in range(1, 101):
